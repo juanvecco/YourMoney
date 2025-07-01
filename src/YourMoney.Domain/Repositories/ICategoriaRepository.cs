5ba69796a05402826c08244a1defdf6caed52ba4
Juan Vecco <juanvecco@gmail.com>
2025-07-01 11:27:00 -0300

    #33 repositories expandidos

---
+++ src/YourMoney.Domain/Repositories/ICategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using YourMoney.Domain.Entities;
+using YourMoney.Domain.Enums;
+
+namespace YourMoney.Domain.Repositories
+{
+    public interface ICategoriaRepository
+    {
+        Task<Categoria> GetByIdAsync(Guid id);
+        Task<List<Categoria>> GetAllAsync();
+        Task<List<Categoria>> GetByTipoAsync(TipoTransacao tipo);
+        Task<List<Categoria>> GetAtivasAsync();
+        Task AdicionarAsync(Categoria categoria);
+        Task AtualizarAsync(Categoria categoria);
+        Task RemoverAsync(Guid id);
+        Task<bool> ExisteAsync(Guid id);
+        Task<bool> ExisteNomeAsync(string nome, TipoTransacao tipo, Guid? ignorarId = null);
+    }
+}
---
+++ src/YourMoney.Domain/Repositories/IReceitaRepository.cs
@@ -0,0 +1,18 @@
+﻿using YourMoney.Domain.Entities;
+
+namespace YourMoney.Domain.Repositories
+{
+    public interface IReceitaRepository
+    {
+        Task<Receita> GetByIdAsync(Guid id);
+        Task<List<Receita>> GetAllAsync();
+        Task<List<Receita>> GetByPeriodoAsync(DateTime dataInicio, DateTime dataFim);
+        Task<List<Receita>> GetByMesAnoAsync(int mes, int ano);
+        Task<List<Receita>> GetByCategoriaAsync(Guid categoriaId);
+        Task<List<Receita>> GetPendentesAsync();
+        Task AdicionarAsync(Receita receita);
+        Task AtualizarAsync(Receita receita);
+        Task RemoverAsync(Guid id);
+        Task<decimal> GetTotalByMesAnoAsync(int mes, int ano);
+    }
+}
