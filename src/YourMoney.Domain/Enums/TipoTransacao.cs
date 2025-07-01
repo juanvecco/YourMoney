3ba8f78c2b4b8dc1824d29b3fdd64cf34e9e3c3e
Juan Vecco <juanvecco@gmail.com>
2025-07-01 10:50:25 -0300

    #27 criar enums tipotransacao

---
+++ src/YourMoney.Domain/Enums/TipoTransacao.cs
@@ -0,0 +1,30 @@
+﻿namespace YourMoney.Domain.Enums
+{
+    public enum TipoTransacao
+    {
+        Receita = 1,
+        Despesa = 2,
+        Investimento = 3,
+        Transferencia = 4
+    }
+
+    public enum StatusMeta
+    {
+        Ativa = 1,
+        Pausada = 2,
+        Concluida = 3,
+        Cancelada = 4
+    }
+
+    public enum TipoRecorrencia
+    {
+        Unica = 0,
+        Semanal = 1,
+        Quinzenal = 2,
+        Mensal = 3,
+        Bimestral = 4,
+        Trimestral = 5,
+        Semestral = 6,
+        Anual = 7
+    }
+}
