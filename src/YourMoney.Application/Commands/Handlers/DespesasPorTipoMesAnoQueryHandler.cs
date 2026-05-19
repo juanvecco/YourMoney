c8cfb8651c6564882176ae46479651ed34ac2a2a
Juan Vecco <juanvecco@gmail.com>
2026-05-19 06:32:46 -0300

    remover arquivos sem uso e add recuperação de senha

---
+++ src/YourMoney.Application/Commands/Handlers/DespesasPorTipoMesAnoQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using YourMoney.Application.Commands.Requests;
+using YourMoney.Application.Queries.Responses;
+using YourMoney.Domain.Repositories;
+
+namespace YourMoney.Application.Commands.Handlers
+{
+    public class DespesasPorTipoMesAnoQueryHandler
+    {
+        private readonly IDespesaRepository _repository;
+
+        public DespesasPorTipoMesAnoQueryHandler(IDespesaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<DespesaResponse>> Handle(DespesasPorTipoMesAnoQuery query)
+        {
+            var despesas = await _repository.GetByTipoMesAnoAsync(
+                query.IdTipoDespesa,
+                query.Mes,
+                query.Ano
+            );
+
+            return despesas.Select(d => new DespesaResponse
+            {
+                Id = d.Id,
+                Descricao = d.Descricao,
+                Valor = d.Valor,
+                Data = d.Data,
+                TipoDespesa = d.TipoDespesa.txtDescricao
+            }).ToList();
+        }
+    }
+
+
+}
